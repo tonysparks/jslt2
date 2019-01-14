3553ed63348aa40b609c65e9442393f1cf96fae2
tonysparks <anthony.sparks31@gmail.com>
2019-01-14 15:47:21 -0600

    Speedier Stack implementation

--- src/main/java/jslt2/util/Stack.java
+++ src/main/java/jslt2/util/Stack.java
@@ -3,8 +3,6 @@
  */
 package jslt2.util;
 
-import java.util.ArrayList;
-import java.util.Collection;
 
 /**
  * Non-synchronized stack
@@ -12,42 +10,94 @@ import java.util.Collection;
  * @author Tony
  *
  */
-public class Stack<T> extends ArrayList<T> {
+public class Stack<T> {
 
-    /**
-     * SUID
-     */
-    private static final long serialVersionUID = 237872682067888089L;
-
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+    
+    private Object[] elements;
+    private int count;
+    
     public Stack() {
-        super();
+        this(16);
     }
 
     /**
      * @param initialCapacity
      */
     public Stack(int initialCapacity) {
-        super(initialCapacity);
+        this.elements = new Object[initialCapacity];
     }
 
-    /**
-     * @param c
-     */
-    public Stack(Collection<T> c) {
-        super(c);
+    private void ensureCapacity(int minCapacity) {
+        if(minCapacity > elements.length) {
+            int oldCapacity = elements.length;
+            int newCapacity = oldCapacity + (oldCapacity >> 1);
+            if (newCapacity - minCapacity < 0)
+                newCapacity = minCapacity;
+            if (newCapacity - MAX_ARRAY_SIZE > 0)
+                newCapacity = hugeCapacity(minCapacity);
+            Object[] newArray = new Object[newCapacity];
+            System.arraycopy(elements, 0, newArray, 0, oldCapacity);
+            elements = newArray;
+        }
+    }
+    
+    private static int hugeCapacity(int minCapacity) {
+        if (minCapacity < 0) // overflow
+            throw new OutOfMemoryError();
+        return (minCapacity > MAX_ARRAY_SIZE) ?
+            Integer.MAX_VALUE :
+            MAX_ARRAY_SIZE;
+    }
+    
+
+    
+    public int size() {
+        return count;
+    }
+    
+    public boolean isEmpty() {
+        return count == 0;
+    }
+    
+    public boolean contains(T element) {
+        for(int i = 0; i < count; i++) {
+            if(elements[i].equals(element)) {
+                return true;
+            }
+        }
+        
+        return false;
     }
     
     public void push(T element) {
-        this.add(element);
+        ensureCapacity(count + 1);
+        elements[count++] = element;
     }
     
+    public void add(T element) {
+        ensureCapacity(count + 1);
+        elements[count++] = element;
+    }
+    
+    @SuppressWarnings("unchecked")
     public T pop() {
-        T result = this.remove(this.size() - 1);
+        if(count < 1) {
+            return null;
+        }
+        
+        T result = (T)elements[--count]; 
         return result;
     }
     
+    @SuppressWarnings("unchecked")
     public T peek() {
-        return get(this.size() - 1);
+        if(count < 1) {
+            return null;
+        }
+        
+        T result = (T)elements[count - 1]; 
+        return result; 
     }
 
 }
